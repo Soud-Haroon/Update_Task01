b06bb825cd29d9360b102d4fe7b58c5aef8ebdbb
Soud <78749228+Soud-Haroon@users.noreply.github.com>
2021-08-26 10:49:41 +0500

    new change

--- lib/mainProfile.dart
+++ lib/mainProfile.dart
@@ -27,7 +27,7 @@ class _MainProfileState extends State<MainProfile> {
         body: ListView(
           children: [
             Container(
-              height: MediaQuery.of(context).size.height * 0.50,
+              height: MediaQuery.of(context).size.height * 0.52,
               child: Stack(
                 children: [
                   //--------------backimage-----------------//
@@ -58,7 +58,7 @@ class _MainProfileState extends State<MainProfile> {
                           SizedBox(height: 5),
                           Text('Front-End & UI',
                               style: TextStyle(color: Colors.grey)),
-                          SizedBox(height: 30),
+                          SizedBox(height: 70),
                         ],
                       ),
                     ),
@@ -77,7 +77,7 @@ class _MainProfileState extends State<MainProfile> {
                           topLeft: Radius.circular(30),
                           topRight: Radius.circular(30),
                         ),
-                        color: Colors.red,
+                        color: Color(0xfff4f4f4),
                       ),
                     ),
                   ),
@@ -85,18 +85,81 @@ class _MainProfileState extends State<MainProfile> {
 
                   //-------------center-card-----------------//
                   Positioned(
-                    top: MediaQuery.of(context).size.height * 0.30,
+                    top: MediaQuery.of(context).size.height * 0.29,
                     left: 0,
                     right: 0,
                     child: Container(
-                      height: 180,
                       margin: EdgeInsets.symmetric(horizontal: 25),
-                      decoration: BoxDecoration(
+                      child: ClipRRect(
                         borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(15),
-                          topRight: Radius.circular(15),
+                          topLeft: Radius.circular(10),
+                          topRight: Radius.circular(10),
                         ),
-                        color: Colors.blue,
+                        child: Container(
+                          height: 140,
+                          // margin: EdgeInsets.symmetric(horizontal: 25),
+                          decoration: BoxDecoration(
+                            border: Border(
+                                bottom: BorderSide(color: darkRed, width: 3)),
+                            color: Colors.blue,
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 20),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Text('Check In',
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.bold)),
+                                    SizedBox(height: 5),
+                                    Text('You Haven,t chock in yet',
+                                        style: TextStyle(color: Colors.grey)),
+                                    SizedBox(height: 5),
+                                    Text('00 : 00 : 00 HRS',
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 18)),
+                                    //----------------------//        
+                                    TextButton(
+                                        onPressed: () {
+                                          // Navigator.of(context).push(
+                                          //     MaterialPageRoute(
+                                          //         builder: (context) => Screen3()));
+                                        },
+                                        child: Text('View History')),
+                                  ],
+                                ),
+                                Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                  Container(
+                                      padding: EdgeInsets.all(5),
+                                      height: 100,
+                                      width: 100,
+                                      decoration: BoxDecoration(
+                                        border:
+                                            Border.all(color: darkRed, width: 3),
+                                        borderRadius: BorderRadius.circular(100),
+                                        color: Colors.white,
+                                      ),
+                                      child: ElevatedButton(
+                                        onPressed: () {},
+                                        child: Text('Check In',
+                                            style: TextStyle(fontSize: 11)),
+                                        style: ElevatedButton.styleFrom(
+                                          shape: CircleBorder(),
+                                          // padding: EdgeInsets.all(14),
+                                        ),
+                                      ))
+                                ])
+                              ],
+                            ),
+                          ),
+                        ),
                       ),
                     ),
                   )
@@ -173,6 +236,35 @@ class _MainProfileState extends State<MainProfile> {
                       itemCount: 3,
                       itemBuilder: (context, index) => EventCard())),
             ),
+            //---------------------------------------------------------//
+            headViewList('Upcoming Holidays'),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(20),
+                  ),
+                  color: Colors.white,
+                ),
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          height: 210,
+                          child: ListView.builder(
+                              physics: NeverScrollableScrollPhysics(),
+                              itemCount: 3,
+                              itemBuilder: (context, index) => LeaveCard()),
+                        ),
+                      ]),
+                ),
+              ),
+            ),
+            SizedBox(height: 50),
           ],
         ),
       ),
